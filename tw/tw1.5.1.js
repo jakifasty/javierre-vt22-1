0a33fd3713ac6748e269200a1777128d5fa1112a
cristi <cristi@kth.se>
2022-01-18 15:40:50 +0100

    not running if file is missing

--- tw/tw1.5.1.js
+++ tw/tw1.5.1.js
@@ -1,13 +1,20 @@
 import render from "./teacherRender.js";
 import dishesConst from "/test/dishesConst.js";
 
-// make webpack load the file only if it exists
-const SidebarView=require("/src/views/"+TEST_PREFIX+"sidebarView.js").default;
+let SidebarView;
+const X= TEST_PREFIX;
+try{
+    SidebarView=require("/src/views/"+X+"sidebarView.js").default;
+}catch(e){
+    render(<div>Please define /src/views/sidebarView.js</div>,  document.getElementById('root'));
+}
+if(SidebarView) {   
 
-function getDishDetails(x){ return dishesConst.find(function(d){ return d.id===x;});}
-
-render(
-        <SidebarView number={3} dishes={[getDishDetails(200), getDishDetails(2), getDishDetails(100), getDishDetails(1)]}
-              onNumberChange={function(){console.log("UI is not interactive, we only test the View here");}}/>,
-    document.getElementById('root')
-);
+    function getDishDetails(x){ return dishesConst.find(function(d){ return d.id===x;});}
+    
+    render(
+            <SidebarView number={3} dishes={[getDishDetails(200), getDishDetails(2), getDishDetails(100), getDishDetails(1)]}
+                         onNumberChange={function(){console.log("UI is not interactive, we only test the View here");}}/>,
+        document.getElementById('root')
+    );
+}    
