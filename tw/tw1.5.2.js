eac4f85ac8e474a35588553652e866418f7d84e1
cristi <cristi@kth.se>
2022-01-18 01:49:18 +0100

    added dish with no type to the tests

--- tw/tw1.5.2.js
+++ tw/tw1.5.2.js
@@ -11,7 +11,9 @@ render(
 window.myModel= require("/src/vuejs/"+TEST_PREFIX+"VueRoot.js").proxyModel;
 function getDishDetails(x){ return dishesConst.find(function(d){ return d.id===x;});}
 
+
 window.myModel.addToMenu(getDishDetails(200));
 window.myModel.addToMenu(getDishDetails(2));
 window.myModel.addToMenu(getDishDetails(100));
+window.myModel.addToMenu(getDishDetails(1));
 window.myModel.setNumberOfGuests(5);
